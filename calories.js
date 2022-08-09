f4edb9a94d40d11567b876fa757df23fc08b0acb
SpeedSTS <42094466+SpeedSTS@users.noreply.github.com>
2022-08-09 20:40:14 +0200

    Add js file
    Mobile menu

--- calories.js
+++ calories.js
@@ -1,1 +1,12 @@
-console.log("Witaj na stronie!");
+
+let hamburgerMenu = document.getElementById("hamburgerMenu");
+let nav = document.querySelector("nav");
+
+hamburgerMenu.addEventListener("click", () => {
+    if(nav.style.display === "none") {
+        nav.style.display = "block"
+    } else {
+        nav.style.display = "none"
+
+    }
+})
